03249e93870f0dc5b1679f5ce17e623153185b1b
Ethan Stancliff <alphagaminggod@gmail.com>
2017-12-21 15:06:46 -0800

    Update Arithmetic_Geometric.py

--- Arithmetic_Geometric.py
+++ Arithmetic_Geometric.py
@@ -15,3 +15,16 @@ Class Predict():
 			return mean
 		else:
 			return 0
+	
+	def Arithmetic(self, pValue): #The geometric sequence predictor
+		data = self.data
+		data_value_differences = []
+		for i in range(len(data)-1):
+			data_value_differences.append(data[i+1]-data[i]) #Gathers the amount you add from one to get the next
+		mean = statistics.mean(data_value_differences) #Calculates the mean of that value
+		sDev = statistics.pstdev(data_value_differences) #Gets the standard deviation from the data_value_differences
+
+		if sDev <= pValue: #Only gives you the value if it's more accurate than what you asked for
+			return mean
+		else:
+			return 0
